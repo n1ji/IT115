ce218289032c58b8d538f33db9789a2a9c826829
Myra <0.maysin.0@gmail.com>
2024-11-14 10:55:04 -0800

    first commit

---
+++ JSON Assignment/json.py
@@ -0,0 +1,40 @@
+import json
+
+dataOne = {
+    'name':'Myra',
+    'ageInYears':22.4011,
+    'city':'Sunnyside, NY',
+    'interests':['Headphones','Technology','Programming','Rock Climbing','Photography'],
+    'isStudent': False
+}
+
+#
+with open('dataOne.json', 'w') as json_file:
+    #Dump the data dictionary to a JSON file
+    json.dump(dataOne,json_file,indent=4)
+
+#Confirmation message that writes to the terminal to let us know the program has run successfully
+print('Data has been written to dataOne.json')
+
+#
+with open ('dataOne.json','r') as json_file:
+    #Load data from json file
+    loadedData = json.load(json_file)
+
+print('Successfully read dataOne.json')
+print(loadedData)
+
+#Change age and add another interest to the list
+loadedData['ageInYears'] = 22.40657
+loadedData['interests'].append('Webfishing')
+
+#Add and remove an interest
+loadedData['interests'].append('R6S')
+loadedData['interests'].remove('R6S')
+
+with open('dataOne.json', 'w') as json_file:
+    #Dump the data dictionary to a JSON file
+    json.dump(loadedData,json_file,indent=4)
+
+#Confirmation message that writes to the terminal to let us know the program has run successfully
+print('Data has been re-written to dataOne.json')
